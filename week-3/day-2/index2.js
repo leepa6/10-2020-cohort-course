a091c92b8037dbb8be9a8265468ab891ff56267a
Patrick Lee <lee.patrick.sung@gmail.com>
2020-11-10 19:58:21 -0800

    again

---
+++ week-3/day-2/index2.js
@@ -0,0 +1,35 @@
+var calcAnswerAdd = function() {
+    console.log('the button click event worked :)');
+    var input1 = document.getElementById("input1").value;
+    var input2 = document.getElementById("input2").value;
+    console.log('input1')
+    var inputsum = parseInt(input1) + parseInt(input2);
+    console.log(inputsum)
+}
+
+var calcAnswerMultiply = function() {
+    console.log('the button click event worked :)');
+    var input3 = document.getElementById("input3").value;
+    var input4 = document.getElementById("input4").value;
+    console.log('input1')
+    var inputmultiply = parseInt(input3) * parseInt(input4);
+    console.log(inputmultiply)
+}
+
+var calcAnswerDivide = function() {
+    console.log('the button click event worked :)');
+    var input5 = document.getElementById("input5").value;
+    var input6 = document.getElementById("input6").value;
+    console.log('input1')
+    var inputdivide = parseInt(input5) / parseInt(input6);
+    console.log(inputdivide)
+}
+
+var calcAnswerSubtract = function() {
+    console.log('the button click event worked :)');
+    var input7 = document.getElementById("input7").value;
+    var input8 = document.getElementById("input8").value;
+    console.log('input1')
+    var inputsubtract = parseInt(input7) - parseInt(input8);
+    console.log(inputsubtract)
+}
