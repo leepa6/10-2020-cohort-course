178ad1a50d0d3d14ed9d1f972577cb279836cb9d
Patrick Lee <lee.patrick.sung@gmail.com>
2020-11-10 17:04:10 -0800

    w3d2.2

--- week-2/day-4/index.js
+++ week-2/day-4/index.js
@@ -1,11 +1,45 @@
-var currentPosition = 0
+var currentPosition = 0;
 
+/*
 var prevImage = function () {
     currentPosition = currentPosition + 500;
-    document.getElementById('imageSlider2').style.transform = 'translateX(' + currentPosition + 'px)'
+    document.getElementById('windowjs').style.transform = 'translateX(' + currentPosition + 'px)'
 }
 
 var nextImage = function () {
     currentPosition = currentPosition - 500;
-    document.getElementById('imageSlider2').style.transform = 'translateX(' + currentPosition + 'px)'
+    document.getElementById('windowjs').style.transform = 'translateX(' + currentPosition + 'px)'
 }
+
+*/
+
+var currentPosition = 0;
+
+function prevImage() {
+    console.log('iwork')
+    if (currentPosition > -1000) {
+        currentPosition -= 500;
+        console.log(currentPosition)
+        document.getElementById(
+            "windowjs"
+        ).style.transform = "translateX(" + currentPosition + "px)";
+    }
+}
+
+
+function prevImage() {
+    console.log('iwork')
+    if (currentPosition < 2500) {
+        currentPosition -= 500;
+        document.getElementById(
+            "windowjs"
+        ).style.transform = "translateX(" + currentPosition + "px)";
+    }
+}
+
+/* js feature . chat box break it down or to do list
+// function prev () {}
+create a task 
+- set up if statement
+check current position
+make sure not out of bounds from positive and negatives
