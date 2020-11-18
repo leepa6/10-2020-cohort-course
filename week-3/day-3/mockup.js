7c662dc540385badd511d75085e2ed9cde073d54
Patrick Lee <lee.patrick.sung@gmail.com>
2020-11-18 01:01:21 -0800

    everything

---
+++ week-3/day-3/mockup.js
@@ -0,0 +1,9 @@
+function addToDo () {
+    var taskToDo = document.createElement('span');
+    taskToDo.innerText = 'take out trash'; //inputting text for to do list item
+    //on click add to do function, grab the text from input 
+    document.getElementById("taskToDo").appendChild(taskToDo).; //once id is ID'd, append child taskToDo (a span)
+    
+    //create a new task item (div) with edit button and delete button functionality 
+
+}
